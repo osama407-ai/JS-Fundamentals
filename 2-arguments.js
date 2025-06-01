a4cbd7838a36bf0a9b1d0dcca2872cf9faebb7e6
LlungileKhoza <llungilekhoza@gmail.com>
2025-06-01 21:50:59 +0200

    Corrected 2-arguments.js file in directory

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,6 +1,6 @@
 const args = Process.argv.slice(2);
 
-if (args.length ===0){
+if (args.length === 0){
     console.log("No argument");
 } else if (args.length === 1){
     console.log("Argument found");
